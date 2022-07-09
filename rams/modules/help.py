6187a2133d07fe738bb2d0715a605e8189a488be
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2022-07-09 14:31:29 +0800

    Update help.py

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -30,11 +30,10 @@ async def help(event):
         await edit_or_reply(
             event,
             f"{EMOJI_HELP}   {string}"
-            f"\n\nSupport @RAM_UBOT\n"
         )
         await event.reply(
             f"╭┄──────┈┄┈──────┄\n"
-            f"│ ▸ **Daftar Perintah RAM-UBOT :**\n"
+            f"│ ▸ **Daftar Perintah FER-UBOT :**\n"
             f"│ ▸ **Jumlah** `{len(modules)}` **Modules**\n"
             f"│ ▸ **Owner:** [{user.first_name}](tg://user?id={user.id})\n"
             f"├┄─────┈┄┈─────┄\n"
