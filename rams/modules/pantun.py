da32a5f805e7e6d1acab47e2a6e0d478d048a2c8
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2022-07-10 11:37:19 +0800

    Update pantun.py

--- rams/modules/pantun.py
+++ rams/modules/pantun.py
@@ -1,1 +1,7 @@
-
+from asyncio import sleep
+from telethon.tl.types import ChatBannedRights
+from telethon.tl.functions.channels import EditBannedRequest
+from rams.events import register
+from rams import CMD_HELP
+from rams import CMD_HANDLER as cmd
+from rams.utils import ram_cmd
