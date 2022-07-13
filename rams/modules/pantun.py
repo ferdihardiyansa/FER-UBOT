c458a35179b53be5a62c8b8a294f2f24be610a27
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2022-07-13 09:20:14 +0800

    Update pantun.py

--- rams/modules/pantun.py
+++ rams/modules/pantun.py
@@ -8,15 +8,15 @@ from rams.utils import edit_or_reply, ram_cmd
 
 @ram_cmd(pattern="sadboy(?: |$)(.*)")
 async def _(event):
-    xx = await edit_or_reply(event, "`Pertama-tama kamu cantik`")
+    xx = await edit_or_reply(event, "`ikan hiu makan tomat`")
     sleep(2)
-    await xx.edit("`Kedua kamu manis`")
+    await xx.edit("`i love you so much`")
     sleep(1)
-    await xx.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
+    await xx.edit("`JIAKHHHH`")
 
 CMD_HELP.update(
     {
-        "arts": f"**Plugin : **`arts`\
+        "Pantun": f"**Plugin : **`pantun`\
         \n\n  •  **Syntax :** `{cmd}kodok`\
         \n  •  **Function : **arts Frog.\
         \n\n  •  **Syntax :** `{cmd}kodog`\
@@ -33,8 +33,8 @@ CMD_HELP.update(
         \n  •  **Function : **arts berfikir\
         \n\n  •  **Syntax :** `{cmd}misi` ; `{cmd}pantau`\
         \n  •  **Function : **Arts Beruang kek lagi mantau.\
-        \n\n  •  **Syntax :** `{cmd}sadboy`\
-        \n  •  **Function : **ya sadboy coba aja.\
+        \n\n  •  **Syntax :** `{cmd}tahu`\
+        \n  •  **Function : **pantun tahu.\
     "
     }
 )
